ff0e115e44041e73cfd96e614d1ff8ece092589f
Sankaragomathi-123 <123377765+Sankaragomathi-123@users.noreply.github.com>
2024-02-20 21:43:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 function findVowelsLengthReverse(str) {
   let vowelsLetter = {};
+  let sumCount = 0;
   const replaceText = str.replace("hero", "vaillan");
+  console.log(replaceText, "replaceText");
 
   for (let i = 0; i < replaceText.length; i++) {
     const char = replaceText[i];
     if ("aeiouAEIOU".includes(char)) {
-      console.log(char, "char");
-
-      vowelsLetter[char] = i;
+      if (vowelsLetter[char]) {
+        vowelsLetter[char]++;
+      } else {
+        vowelsLetter[char] = 1;
+      }
     }
   }
-  return vowelsLetter;
+  const value = Object.values(vowelsLetter);
+  for (let i = 0; i < value.length; i++) {
+    console.log(value[i], "nit");
+    sumCount += value[i];
+  }
+  console.log(sumCount, "sumCount");
+  return sumCount;
 }
 
 const text = "My dad is super hero";
